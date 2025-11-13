14df39a3766cd7abdfebdb93b249e1a70b44ceab
Eduard Grebe <eduard@grebe.consulting>
2025-11-13 20:21:11 +0200

    Add copyright and GPL license notice

--- Inctools/src/Inctools.jl
+++ Inctools/src/Inctools.jl
@@ -1,5 +1,14 @@
-# Incidence Estimation Tools (Julia implementation). Copyright (C) 2019,
-# Eduard Grebe and individual contributors.
+# Incidence Estimation Tools (Julia implementation with R interface).
+# Copyright (C) 2015-2025, Stellenbosch University, Vitalant,
+# Eduard Grebe, and other inddividual contributors.
+# Primary author and maintainer: Eduard Grebe <eduard@grebe.consulting>
+# Alternative email addresses: <egrebe@vitalant.org> <eduard.grebe@ucsf.edu>
+# AI coding assistance by Claude AI (Anthropic)
+# Based on original implementation of incidence estimation methods in the
+# R package inctools by Alex Welte, Eduard Grebe, Avery McIntosh,
+# Petra Bäumler, Simon Daniel and Yuruo Li, with contributions by
+# Cari van Schalkwyk, Reshma Kassanjee, Hilmarie Brand,
+# Stefano Ongarello and Yusuke Asai.
 #
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
--- tests/test_covar_deprecation.jl
+++ tests/test_covar_deprecation.jl
@@ -1,3 +1,24 @@
+# Incidence Estimation Tools (Julia implementation with R interface).
+# Copyright (C) 2015-2025, Stellenbosch University, Vitalant,
+# Eduard Grebe, and other inddividual contributors.
+# Primary author and maintainer: Eduard Grebe <eduard@grebe.consulting>
+# Alternative email addresses: <egrebe@vitalant.org> <eduard.grebe@ucsf.edu>
+# AI coding assistance by Claude AI (Anthropic)
+# Based on original implementation of incidence estimation methods in the
+# R package inctools by Alex Welte, Eduard Grebe, Avery McIntosh,
+# Petra Bäumler, Simon Daniel and Yuruo Li, with contributions by
+# Cari van Schalkwyk, Reshma Kassanjee, Hilmarie Brand,
+# Stefano Ongarello and Yusuke Asai.
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.  This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.  You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 using Pkg
 
 # Find Inctools package (works from repository root or tests/ directory)
